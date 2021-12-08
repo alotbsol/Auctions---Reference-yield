cfa33d515192d811f9cae6dd215e3bb7b3ff98b8
alotbsol <prokop.cech@seznam.cz>
2021-12-08 20:36:28 +0100

    first steps

--- fun.py
+++ fun.py
@@ -8,17 +8,36 @@ def ran_gen_float(lower_limit, upper_limit, size=1):
     return x[0]
 
 
-# average function
-def average(lst):
-    return sum(lst) / len(lst)
+testing_probability = {"limits": [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1],
+                       "values": [4.96961689, 6.092707157, 6.582392311, 6.893632889, 7.27610836,
+                                  7.583831787, 7.791398525, 7.964614773, 8.286667824, 8.537548065, 8.914340019],
+                      }
 
 
-# Random generation of x numbers
-def ran_gen_uni(lower_limit, upper_limit, size):
-    x = np.random.uniform(int(lower_limit), int(upper_limit), int(size))
-    return list(x)
+def random_from_prob_dist(input_probabilities):
+    random_var = np.random.random()
+    limits_array = np.array(input_probabilities["limits"])
 
-def ran_int(lower_limit, upper_limit, size):
-    return list(randint(lower_limit, upper_limit + 1, size))
+    closest_lower = limits_array[limits_array < random_var].max()
+    closest_higher = limits_array[limits_array > random_var].min()
+
+    index_lower = input_probabilities["limits"].index(closest_lower)
+    index_higher = input_probabilities["limits"].index(closest_higher)
+
+    return_value = input_probabilities["values"][index_higher] - input_probabilities["values"][index_lower]
 
 
+
+    print(limits_array)
+    print(random_var)
+    print(closest_lower)
+    print(closest_higher)
+    print(index_lower)
+    print(index_higher)
+    print(return_value)
+
+
+
+
+
+random_from_prob_dist(testing_probability)
--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import projects
 import power_curves
 from fun import ran_gen_float
 from german_auctions import auctions_supply_demand
-from storage import Storage
 
 
 def scenario1():
@@ -62,10 +61,11 @@ def scenario3_supply_demand():
     df_results = pd.DataFrame()
 
     number_of_projects = 41
-    ref_yield_scenarios = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.1, 1.2, 1.3, 1.4, 1.5]
+    ref_yield_scenarios = [0, 0.25, 0.5, 0.75, 1, 1.25, 1.5]
+    demand_scenarios = [1, 5, 10, 15, 20, 25, 30, 35, 40, 41]
 
     # submitted/won/max bid/average project
-    for ii in range(1, number_of_projects+1):
+    for ii in demand_scenarios:
         demand = ii
 
         Master_storage = projects.ProjectsStorage(demand=demand, name="{0}".format(str(ii)),
@@ -75,17 +75,14 @@ def scenario3_supply_demand():
         ws_list = np.linspace(start=5, stop=9, num=number_of_projects, endpoint=True, retstep=False, dtype=None)
 
         for i in range(number_of_projects):
-            other_costs = ran_gen_float(lower_limit=0.8, upper_limit=1.2)
-            other_prod = ran_gen_float(lower_limit=0.9, upper_limit=1.1)
-
             Master_storage.add_project(base_lcoe=50,
                                        ws100=ws_list[i],
                                        hub_height=128,
                                        installed_capacity=3,
                                        power_curve=power_curves.Enercon_E115,
                                        turbine_name="Enercon_E115",
-                                       other_cost=other_costs,
-                                       other_production=other_prod)
+                                       other_cost=1,
+                                       other_production=1)
 
         Master_storage.auction_results()
         Master_storage.delete_projects()
@@ -101,11 +98,12 @@ def scenario3_supply_demand():
 def scenario4_other_costs():
     df_results = pd.DataFrame()
 
-    iterations = 1000
+    iterations = 10000
     number_of_projects = 41
-    ref_yield_scenarios = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.1, 1.2, 1.3, 1.4, 1.5]
+    ref_yield_scenarios = [0, 0.25, 0.5, 0.75, 1, 1.25, 1.5]
+    demand_scenarios = [1, 5, 10, 15, 20, 25, 30, 35, 40, 41]
 
-    for iii in range(1, number_of_projects+1):
+    for iii in demand_scenarios:
         demand = iii
 
         Master_storage = projects.ProjectsStorage(demand=demand, name="scenario4_other_costs",
@@ -138,7 +136,49 @@ def scenario4_other_costs():
     writer.save()
 
 
-def scenario5_german_auctions():
+def scenario5_other_costs_production():
+    df_results = pd.DataFrame()
+
+    iterations = 10000
+    number_of_projects = 41
+    ref_yield_scenarios = [0, 0.25, 0.5, 0.75, 1, 1.25, 1.5]
+    demand_scenarios = [1, 5, 10, 15, 20, 25, 30, 35, 40, 41]
+
+    for iii in demand_scenarios:
+        demand = iii
+
+        Master_storage = projects.ProjectsStorage(demand=demand, name="scenario5_other_costs_production",
+                                                  ref_yield_scenarios=ref_yield_scenarios,
+                                                  project_size_adjustment=3)
+
+        for ii in range(iterations):
+            ws_list = np.linspace(start=5, stop=9, num=number_of_projects, endpoint=True, retstep=False, dtype=None)
+
+            for i in range(number_of_projects):
+                other_costs = ran_gen_float(lower_limit=0.8, upper_limit=1.2)
+                other_prod = ran_gen_float(lower_limit=0.9, upper_limit=1.1)
+
+                Master_storage.add_project(base_lcoe=50,
+                                           ws100=ws_list[i],
+                                           hub_height=128,
+                                           installed_capacity=3,
+                                           power_curve=power_curves.Enercon_E115,
+                                           turbine_name="Enercon_E115",
+                                           other_cost=other_costs,
+                                           other_production=other_prod)
+
+            Master_storage.auction_results()
+            Master_storage.delete_projects()
+            Master_storage.iteration += 1
+
+        df_results = df_results.append(Master_storage.return_results())
+
+    writer = pd.ExcelWriter("scenario5_other_costs_production.xlsx", engine="xlsxwriter")
+    df_results.to_excel(writer, sheet_name="Results")
+    writer.save()
+
+
+def scenario6_german_auctions():
     df_results = pd.DataFrame()
     iterations = 10000
     ref_yield_scenarios = [0, 1, 1.5]
@@ -178,33 +218,10 @@ def scenario5_german_auctions():
 
         df_results = df_results.append(Master_storage.return_results())
 
-    writer = pd.ExcelWriter("scenario5_german_auctions.xlsx", engine="xlsxwriter")
+    writer = pd.ExcelWriter("scenario6_german_auctions.xlsx", engine="xlsxwriter")
     df_results.to_excel(writer, sheet_name="Results")
     writer.save()
 
-
-def test():
-    number_of_projects = 3
-    ref_yield_scenarios = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.1, 1.2, 1.3, 1.4, 1.5]
-    """[0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.1, 1.2, 1.3, 1.4, 1.5]"""
-
-    Master_storage = projects.ProjectsStorage(demand=4, name="test",
-                                              ref_yield_scenarios=ref_yield_scenarios,)
-
-    ws_list = np.linspace(start=5, stop=9, num=number_of_projects, endpoint=True, retstep=False, dtype=None)
-
-    for i in range(number_of_projects):
-        Master_storage.add_project(base_lcoe=50,
-                                   ws100=ws_list[i],
-                                   hub_height=128,
-                                   installed_capacity=3,
-                                   power_curve=power_curves.Enercon_E115,
-                                   turbine_name="Enercon_E115",
-                                   other_cost=1.8,
-                                   other_production=1)
-
-    Master_storage.auction_results()
-    Master_storage.export(projects_export=True)
 
 
 if __name__ == '__main__':
@@ -214,15 +231,18 @@ if __name__ == '__main__':
     """
     scenario1()
     scenario2_other_costs()
-    scenario3_german_auctions()
-    test()
-    scenario3_german_auctions()
+
+
+    scenario6_german_auctions()
     scenario2()
 
     scenario3_supply_demand()
-    """
+    
 
     scenario4_other_costs()
+    """
+
+
 
     end_time = datetime.now()
     print("calculation ends")
