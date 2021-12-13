4d02cc5f82097e634a99fadef697954849656d7c
alotbsol <prokop.cech@seznam.cz>
2021-12-13 22:19:03 +0100

    first steps

--- main.py
+++ main.py
@@ -34,32 +34,8 @@ def scenario1():
     Master_storage.auction_results()
     Master_storage.export(projects_export=True)
 
-def scenario2():
-    number_of_projects = 41
-    ref_yield_scenarios = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.1, 1.2, 1.3, 1.4, 1.5]
-    """[0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.1, 1.2, 1.3, 1.4, 1.5]"""
 
-    Master_storage = projects.ProjectsStorage(demand=31, name="scenario2",
-                                              ref_yield_scenarios=ref_yield_scenarios,
-                                              project_size_adjustment=3)
-
-    ws_list = np.linspace(start=5, stop=9, num=number_of_projects, endpoint=True, retstep=False, dtype=None)
-
-    for i in range(number_of_projects):
-        Master_storage.add_project(base_lcoe=50,
-                                   ws100=ws_list[i],
-                                   hub_height=128,
-                                   installed_capacity=3,
-                                   power_curve=power_curves.Enercon_E115,
-                                   turbine_name="Enercon_E115",
-                                   other_cost=1,
-                                   other_production=1)
-
-    Master_storage.auction_results()
-    Master_storage.export(projects_export=True)
-
-
-def scenario3_supply_demand():
+def scenario2_supply_demand():
     df_results = pd.DataFrame()
 
     number_of_projects = 41
@@ -92,23 +68,24 @@ def scenario3_supply_demand():
 
         df_results = df_results.append(Master_storage.return_results())
 
-    writer = pd.ExcelWriter("scenario3_supply_demand.xlsx", engine="xlsxwriter")
+    writer = pd.ExcelWriter("scenario2_supply_demand.xlsx", engine="xlsxwriter")
     df_results.to_excel(writer, sheet_name="Results")
     writer.save()
 
 
-def scenario4_other_costs():
+def scenario3_other_costs():
     df_results = pd.DataFrame()
 
-    iterations = 10000
+    iterations = 1000
     number_of_projects = 41
     ref_yield_scenarios = [0, 0.25, 0.5, 0.75, 1, 1.25, 1.5]
     demand_scenarios = [1, 5, 10, 15, 20, 25, 30, 35, 40, 41]
 
     for iii in demand_scenarios:
+        print("demand scenario:", iii)
         demand = iii
 
-        Master_storage = projects.ProjectsStorage(demand=demand, name="scenario4_other_costs",
+        Master_storage = projects.ProjectsStorage(demand=demand, name="scenario3_other_costs",
                                                   ref_yield_scenarios=ref_yield_scenarios,
                                                   project_size_adjustment=3)
 
@@ -133,12 +110,12 @@ def scenario4_other_costs():
 
         df_results = df_results.append(Master_storage.return_results())
 
-    writer = pd.ExcelWriter("scenario4_other_costs.xlsx", engine="xlsxwriter")
+    writer = pd.ExcelWriter("scenario3_other_costs.xlsx", engine="xlsxwriter")
     df_results.to_excel(writer, sheet_name="Results")
     writer.save()
 
 
-def scenario5_other_costs_production():
+def scenario4_other_costs_production():
     df_results = pd.DataFrame()
 
     iterations = 10000
@@ -148,8 +125,9 @@ def scenario5_other_costs_production():
 
     for iii in demand_scenarios:
         demand = iii
+        print("demand scenario:", demand)
 
-        Master_storage = projects.ProjectsStorage(demand=demand, name="scenario5_other_costs_production",
+        Master_storage = projects.ProjectsStorage(demand=demand, name="scenario4_other_costs_production",
                                                   ref_yield_scenarios=ref_yield_scenarios,
                                                   project_size_adjustment=3)
 
@@ -175,25 +153,24 @@ def scenario5_other_costs_production():
 
         df_results = df_results.append(Master_storage.return_results())
 
-    writer = pd.ExcelWriter("scenario5_other_costs_production.xlsx", engine="xlsxwriter")
+    writer = pd.ExcelWriter("scenario4_other_costs_production.xlsx", engine="xlsxwriter")
     df_results.to_excel(writer, sheet_name="Results")
     writer.save()
 
 
-def scenario6_german_auctions():
-    writer = pd.ExcelWriter("scenario6_german_auctions.xlsx", engine="xlsxwriter")
+def scenario5_german_auctions():
+    writer = pd.ExcelWriter("scenario5_german_auctions.xlsx", engine="xlsxwriter")
 
     df_results = pd.DataFrame()
-    iterations = 5000
+    iterations = 10000
     ref_yield_scenarios = [0, 1, 1.5]
 
     distributions = [german_auctions.uniform, german_auctions.A_2015_to_2018]
 
     for iv in distributions:
-
+        print(iv)
     # submitted/won/max bid/average project
         for iii in auctions_supply_demand:
-
             number_of_projects = auctions_supply_demand[iii][0]
             demand = auctions_supply_demand[iii][1]
             max_bid_possible = auctions_supply_demand[iii][2]
@@ -238,7 +215,7 @@ if __name__ == '__main__':
     print("calculation starts")
     start_time = datetime.now()
 
-    scenario6_german_auctions()
+    scenario4_other_costs_production()
 
     end_time = datetime.now()
     print("calculation ends")
