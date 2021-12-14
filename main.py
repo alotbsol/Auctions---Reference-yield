4887825e4f7b073060bc5e3596aa44793ec5f965
alotbsol <prokop.cech@seznam.cz>
2021-12-14 23:30:40 +0100

    auction scenarios

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def scenario2_supply_demand():
     df_results = pd.DataFrame()
 
     number_of_projects = 41
-    ref_yield_scenarios = [0, 0.25, 0.5, 0.75, 1, 1.25, 1.5]
+    ref_yield_scenarios = [0, 0.5, 0.99, 1.01, 1.5]
     demand_scenarios = [1, 5, 10, 15, 20, 25, 30, 35, 40, 41]
 
     # submitted/won/max bid/average project
@@ -78,7 +78,7 @@ def scenario3_other_costs():
 
     iterations = 1000
     number_of_projects = 41
-    ref_yield_scenarios = [0, 0.25, 0.5, 0.75, 1, 1.25, 1.5]
+    ref_yield_scenarios = [0, 0.5, 0.99, 1.01, 1.5]
     demand_scenarios = [1, 5, 10, 15, 20, 25, 30, 35, 40, 41]
 
     for iii in demand_scenarios:
@@ -115,12 +115,13 @@ def scenario3_other_costs():
     writer.save()
 
 
+"""NOT USED FOR THE ANALYSIS"""
 def scenario4_other_costs_production():
     df_results = pd.DataFrame()
 
     iterations = 10000
     number_of_projects = 41
-    ref_yield_scenarios = [0, 0.25, 0.5, 0.75, 1, 1.25, 1.5]
+    ref_yield_scenarios = [0, 0.5, 0.99, 1.01, 1.5]
     demand_scenarios = [1, 5, 10, 15, 20, 25, 30, 35, 40, 41]
 
     for iii in demand_scenarios:
@@ -158,14 +159,14 @@ def scenario4_other_costs_production():
     writer.save()
 
 
-def scenario5_german_auctions():
+def scenario5_german_auctions_average():
     writer = pd.ExcelWriter("scenario5_german_auctions.xlsx", engine="xlsxwriter")
 
     df_results = pd.DataFrame()
     iterations = 10000
     ref_yield_scenarios = [0, 1, 1.5]
 
-    distributions = [german_auctions.uniform, german_auctions.A_2015_to_2018]
+    distributions = [german_auctions.A_2015_to_2018]
 
     for iv in distributions:
         print(iv)
@@ -207,6 +208,113 @@ def scenario5_german_auctions():
             df_results = df_results.append(Master_storage.return_results())
 
         df_results.to_excel(writer, sheet_name=(iv["name"]))
+        df_results = pd.DataFrame()
+
+    writer.save()
+
+def scenario6_german_auctions_uniform():
+    writer = pd.ExcelWriter("scenario6_german_auctions_uniform.xlsx", engine="xlsxwriter")
+
+    df_results = pd.DataFrame()
+    iterations = 10000
+    ref_yield_scenarios = [0, 1, 1.5]
+
+    distributions = [german_auctions.uniform]
+
+    for iv in distributions:
+        print(iv)
+    # submitted/won/max bid/average project
+        for iii in auctions_supply_demand:
+            number_of_projects = auctions_supply_demand[iii][0]
+            demand = auctions_supply_demand[iii][1]
+            max_bid_possible = auctions_supply_demand[iii][2]
+            project_size_adjustment = auctions_supply_demand[iii][3]
+
+            Master_storage = projects.ProjectsStorage(demand=demand, name="{0}".format(str(iii)),
+                                                      ref_yield_scenarios=ref_yield_scenarios,
+                                                      max_bid_possible=max_bid_possible,
+                                                      project_size_adjustment=project_size_adjustment,
+                                                      ws_dist=iv["name"])
+
+            for ii in range(iterations):
+                ws_list = []
+                for w in range(number_of_projects):
+                    ws_list.append(random_from_prob_dist(input_probabilities=iv))
+
+                for i in range(number_of_projects):
+                    other_costs = ran_gen_float(lower_limit=0.8, upper_limit=1.2)
+                    other_prod = ran_gen_float(lower_limit=0.9, upper_limit=1.1)
+
+                    Master_storage.add_project(base_lcoe=50,
+                                               ws100=ws_list[i],
+                                               hub_height=128,
+                                               installed_capacity=3,
+                                               power_curve=power_curves.Enercon_E115,
+                                               turbine_name="Enercon_E115",
+                                               other_cost=other_costs,
+                                               other_production=other_prod)
+
+                Master_storage.auction_results()
+                Master_storage.delete_projects()
+                Master_storage.iteration += 1
+
+            df_results = df_results.append(Master_storage.return_results())
+
+        df_results.to_excel(writer, sheet_name=(iv["name"]))
+        df_results = pd.DataFrame()
+
+    writer.save()
+
+def scenario7_german_auctions_quartals():
+    writer = pd.ExcelWriter("scenario7_german_auctions_quartals.xlsx", engine="xlsxwriter")
+
+    df_results = pd.DataFrame()
+    iterations = 10000
+    ref_yield_scenarios = [0, 1, 1.5]
+
+    distributions = german_auctions.quartals
+
+    for iv in distributions:
+        print(iv)
+    # submitted/won/max bid/average project
+        for iii in auctions_supply_demand:
+            number_of_projects = auctions_supply_demand[iii][0]
+            demand = auctions_supply_demand[iii][1]
+            max_bid_possible = auctions_supply_demand[iii][2]
+            project_size_adjustment = auctions_supply_demand[iii][3]
+
+            Master_storage = projects.ProjectsStorage(demand=demand, name="{0}".format(str(iii)),
+                                                      ref_yield_scenarios=ref_yield_scenarios,
+                                                      max_bid_possible=max_bid_possible,
+                                                      project_size_adjustment=project_size_adjustment,
+                                                      ws_dist=iv["name"])
+
+            for ii in range(iterations):
+                ws_list = []
+                for w in range(number_of_projects):
+                    ws_list.append(random_from_prob_dist(input_probabilities=iv))
+
+                for i in range(number_of_projects):
+                    other_costs = ran_gen_float(lower_limit=0.8, upper_limit=1.2)
+                    other_prod = ran_gen_float(lower_limit=0.9, upper_limit=1.1)
+
+                    Master_storage.add_project(base_lcoe=50,
+                                               ws100=ws_list[i],
+                                               hub_height=128,
+                                               installed_capacity=3,
+                                               power_curve=power_curves.Enercon_E115,
+                                               turbine_name="Enercon_E115",
+                                               other_cost=other_costs,
+                                               other_production=other_prod)
+
+                Master_storage.auction_results()
+                Master_storage.delete_projects()
+                Master_storage.iteration += 1
+
+            df_results = df_results.append(Master_storage.return_results())
+
+        df_results.to_excel(writer, sheet_name=(iv["name"]))
+        df_results = pd.DataFrame()
 
     writer.save()
 
@@ -215,7 +323,7 @@ if __name__ == '__main__':
     print("calculation starts")
     start_time = datetime.now()
 
-    scenario4_other_costs_production()
+    scenario5_german_auctions_average()
 
     end_time = datetime.now()
     print("calculation ends")
